a533c9bacbf8a214f6b215e5fe186d9b74457ff5
Vector254 <ochrist7@gmail.com>
2020-09-29 11:00:50 +0300

    create app configuration

---
+++ app/main/views.py
@@ -0,0 +1,25 @@
+from flask import render_template, redirect, url_for,abort,request
+from . import main
+
+@main.route('/')
+def index():
+    title="Welcome to Job hunter!"
+    
+
+    return render_template('index.html', title=title)
+
+
+@main.route('/hire/new/', methods = ['GET','POST'])
+@login_required
+
+def new_hire():
+    form = Hire()
+    if form.validate_on_submit():
+        location = form.location.data
+        language = form.language.data
+       
+        new_hire = Hiring(location=location, language=language)
+        new_hire.save_hire()
+
+        return redirect(url_for('main.index'))
+    return render_template('hire.html',form=form)
