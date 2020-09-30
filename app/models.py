675ca8cc1fbcbd4919624a07a74eb4e8e6bc5116
Vector254 <ochrist7@gmail.com>
2020-09-30 09:56:42 +0300

    create job class

--- app/models.py
+++ app/models.py
@@ -8,4 +8,17 @@ class Hiring:
         self.id = id
         self.location=location
         self.language = language
+
+class Jobs:
+    '''
+    Articles class to define articles objects
+    '''
+    def __init__(self,id,title,company,type,url,location):
+        self.id = id
+        self.title = title
+        self.company = company
+        self.type= type
+        self.url = url
+        self.location = location
+       
         
