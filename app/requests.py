51ba27dc66922ae8fab96afad982f29d8d9dcb88
Vector254 <ochrist7@gmail.com>
2020-10-01 11:10:31 +0300

    format time display

--- app/requests.py
+++ app/requests.py
@@ -1,5 +1,7 @@
 import urllib.request,json
 from .models import Hiring,Jobs
+from time import ctime
+from datetime import datetime
 
 base_url=None
 
@@ -58,7 +60,7 @@ def process_jobs(jobs_list):
         company_logo = job_item.get('company_logo')
         url = job_item.get('url')
         location = job_item.get('location')
-        date = job_item.get('created_at')
+        date = datetime.strptime (job_item.get('created_at'), '%a %b %d %H:%M:%S %Z %Y')
         description = job_item.get('description')
 
         jobs_result = Jobs(id,time,title,company,company_logo,url,location,date,description)
